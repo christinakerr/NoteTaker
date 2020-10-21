af7ccfd5ec12ee0b73992c4ebf70b8d4c931248e
Christi Kerr <christinakerr394@gmail.com>
2020-10-21 14:37:14 -0500

    Built HTML route for GET /notes Fixes #1

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+// DEPENDENCIES
 const express = require("express");
-const app = express();
 
+// SET UP EXPRESS APP
+const app = express();
+var PORT = 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================
+
+// ROUTES
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "nodes.html"));
+});
